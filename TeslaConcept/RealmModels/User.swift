f5bd25dd8090e7f8e401aa41c764faa471860a54
Kirill Khudyakov <hudiakov@gmail.com>
2020-10-03 14:31:32 +0300

    [+] User

---
+++ TeslaConcept/RealmModels/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  TeslaConcept
+//
+//  Created by Kirill Khudiakov on 03.10.2020.
+//  Copyright © 2020 Kirill Khudiakov. All rights reserved.
+//
+
+import RealmSwift
+
+
+class User: Object {
+    
+    @objc dynamic var id: String = UUID().uuidString
+    @objc dynamic var login = ""
+    @objc dynamic var password = ""
+    @objc dynamic var lastLoggedDate = Date()
+    
+    override static func primaryKey() -> String? {
+          return "id"
+    }
+}
